e59bf4024254e20d0af33b10b681575b7937412e
Leonardo Kim <dalinaum@gmail.com>
2012-02-07 09:28:53 +0900

    remove autogenerated files

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 4 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Wed Jan 18 20:38:41 KST 2012"
-#define LINUX_COMPILE_TIME "20:38:41"
-#define LINUX_COMPILE_BY "leonardo"
-#define LINUX_COMPILE_HOST "deepblue"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
